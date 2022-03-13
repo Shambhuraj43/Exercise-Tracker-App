3bfa6c9b0252568054804deef3de8623782d5f2c
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-03-12 19:41:48 -0500

    Initial Deployment

---
+++ src/components/create-user.component.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateUser extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //binding 'this' class to make sure 
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+        }
+    }
+
+    //if username changes, this function gets called to update values
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    //on Submit method
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        }
+
+        console.log(user);
+
+        //send post request with the created user
+        axios.post('http://localhost:3000/users/add', user)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            username: ''
+        });
+    }
+
+    //simple form for user creation
+    render() {
+        return (
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+//Exercise Component
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
+
+//ExerciseList Component
+export default class ExerciseList extends Component {
+    
+    constructor(props){
+        super(props);
+
+        //binding 'this' object 
+        this.deleteExercise = this.deleteExercise.bind(this);
+        
+        this.state = {exercises: []}; 
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/exercises/')
+        .then(response => {
+            this.setState({exercises: response.data})
+        })
+        .catch((error => {
+            console.log(error);
+        }))
+    }
+
+    //delete method to delete the specific exercise with 'id' from the database
+    deleteExercise(id) {
+      axios.delete('http://localhost:3000/exercises/'+id)
+        .then(response => { console.log(response.data)});
+
+        //cheeky trick : using filter and (not returning the object with 'id')
+        this.setState({
+            exercises: this.state.exercises.filter(el => el._id !== id)
+        })
+    }
+    
+    
+    //exercise list method: lists the rows in the exercise
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+          return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+        })
+      }
+
+
+    render() {
+        return (
+          <div>
+            <h3>Logged Exercises</h3>
+            <table className="table">
+              <thead className="thead-light">
+                <tr>
+                  <th>Username</th>
+                  <th>Description</th>
+                  <th>Duration</th>
+                  <th>Date</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                { this.exerciseList() }
+              </tbody>
+            </table>
+          </div>
+        )
+      }
+    }
