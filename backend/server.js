a4d3c22a7181b9807539b69f2d4c8ec666d03fe9
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-03-12 23:24:12 -0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.listen(port, () => {
 });
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static('../build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'backend', 'build', 'index.html'));
